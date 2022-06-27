c877806c34bb720ce3a85eb26412d441e567370e
reighpuy <daffykhadaffy18@gmail.com>
2022-06-27 13:34:16 +0700

    ubah menu: 2

--- plugins/menu.js
+++ plugins/menu.js
@@ -83,9 +83,9 @@ Howbucin <text>
 let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
   try {
     // let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
-    // let { exp, limit, level, role } = global.db.data.users[m.sender]
-    // let { min, xp, max } = xpRange(level, global.multiplier)
-    // let name = await conn.getName(m.sender)
+    let { exp, limit, level, role } = global.db.data.users[m.sender]
+    let { min, xp, max } = xpRange(level, global.multiplier)
+    let name = await conn.getName(m.sender)
     // let d = new Date(new Date + 3600000)
     // let locale = 'id'
     // // d.getTimeZoneOffset()
